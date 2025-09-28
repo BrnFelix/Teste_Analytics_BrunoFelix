7a4d1a9ca440f33209c6bf10c90948caf2a97bf9
bruno <brunofelix120@gmail.com>
2025-09-28 16:44:07 -0300

    Inserindo os gráficos

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 import random
 from datetime import datetime, timedelta
+
 
 # 1. Simulação do Dataset
 np.random.seed(42) # Para gerar valores aleatórios e fixos, para facilitar na análise dos dados
@@ -69,10 +72,58 @@ valor_top = vendas_por_prato.max()
 # 6. Resultados
 
 print("Tabela com as informações:")
-print(df)
+#print(df)
 
 print(f"\n#####################")
 
 print(f"\nTotal de vendas por prato:")
-print(vendas_por_prato)
-print(f"\n Prato mais lucrativo: {prato_top} (R$ {valor_top:.2f})")
+#print(vendas_por_prato)
+#print(f"\n Prato mais lucrativo: {prato_top} (R$ {valor_top:.2f})")
+
+
+
+# ------- Análise exploratória -------------
+
+
+# Agrupando por mês
+df['AnoMes'] = df['Data'].dt.to_period('M')
+vendas_mensais = df.groupby('AnoMes')['Total_Vendas'].sum()
+
+# Gráfico de linha
+plt.figure(figsize=(12, 6))
+vendas_mensais.plot(kind='line', marker='o', color='teal')
+plt.title('📊 Tendência de Vendas Mensais no Restaurante (2023)', fontsize=14)
+plt.xlabel('Mês')
+plt.ylabel('Total de Vendas (R$)')
+plt.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
+# Total de vendas por categoria
+vendas_por_categoria = df.groupby('Categoria')['Total_Vendas'].sum().sort_values()
+
+plt.figure(figsize=(10, 6))
+ax = sns.barplot(x=vendas_por_categoria.values, y=vendas_por_categoria.index, palette='viridis')
+
+# Adicionando os valores exatos nas barras
+for i, valor in enumerate(vendas_por_categoria.values):
+    ax.text(valor + 5, i, f'R$ {valor:,.2f}', va='center', fontsize=10)
+
+plt.title('💰 Total de Vendas por Categoria Gastronômica')
+plt.xlabel('Total de Vendas (R$)')
+plt.ylabel('Categoria')
+plt.tight_layout()
+plt.show()
+
+vendas_por_prato_pct = df.groupby('Prato')['Total_Vendas'].sum()
+plt.figure(figsize=(8, 8))
+plt.pie(vendas_por_prato_pct, labels=vendas_por_prato_pct.index, autopct='%1.1f%%', startangle=140, colors=sns.color_palette('pastel'))
+plt.title('🍕 Participação dos Pratos nas Vendas Totais')
+plt.axis('equal')
+plt.show()
+
+df['AnoMes'] = df['Data'].dt.to_period('M')
+vendas_mensais = df.groupby('AnoMes')['Total_Vendas'].sum()
+
